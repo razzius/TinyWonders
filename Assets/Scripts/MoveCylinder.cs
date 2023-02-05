bb4f777a2632e29f54a4e24827a520750502db59
Razzi Abuissa <razzi@abuissa.net>
2023-02-04 22:12:03 -0600

    Tank control bugger

--- Assets/Scripts/MoveCylinder.cs
+++ Assets/Scripts/MoveCylinder.cs
@@ -2,19 +2,41 @@ using UnityEngine;
 
 public class MoveCylinder : MonoBehaviour
 {
+    
+
     public float speed = 4f;
     private Quaternion startRotation;
     public GameObject leftMandible2;
     public GameObject rightMandible2;
+    public Camera _mainCamera;
+    public Rigidbody rigidBody;
     void Start()
     {
+        rigidBody = GetComponent<Rigidbody>();
         startRotation = leftMandible2.transform.localRotation;
     }
     // Update is called once per frame
-    void Update()
-    {
-	    float xDirection = Input.GetAxis("Horizontal");
-	    float zDirection = Input.GetAxis("Vertical");
+    // void Update()
+    // {
+	//     float xDirection = Input.GetAxis("Horizontal");
+	//     float zDirection = Input.GetAxis("Vertical");
+
+    //     if (Input.GetKey(KeyCode.A))
+    //     {
+    //         transform.RotateAround(gameObject.transform.position, Vector3.down, 20 * Time.deltaTime);
+    //     }
+	// 	Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
+
+	// 	transform.position += moveDirection * Time.deltaTime * speed;
+    // }
+    private void Update()
+     {
+         Vector3 movDir;
+ 
+         transform.Rotate(0, Input.GetAxis("Horizontal") * 90 * Time.deltaTime, 0);
+         movDir = transform.forward * Input.GetAxis("Vertical") * 6;
+         rigidBody.velocity = movDir;
+         
         if (Input.GetKey(KeyCode.Space)) {
             leftMandible2.transform.localEulerAngles = new Vector3(10.586f, -4.0f, -88.34f);
             rightMandible2.transform.localEulerAngles = new Vector3(0, 30, 0);
@@ -22,8 +44,8 @@ public class MoveCylinder : MonoBehaviour
             leftMandible2.transform.localEulerAngles = new Vector3(10.586f, 14.507f, -88.34f);
             rightMandible2.transform.localEulerAngles = new Vector3(0, 0, 0);
         }
-		Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
-
-		transform.position += moveDirection * Time.deltaTime * speed;
-    }
+        //  float _targetRotation = Mathf.Atan2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Mathf.Rad2Deg +
+        //                           _mainCamera.transform.eulerAngles.y;
+         
+     }
 }

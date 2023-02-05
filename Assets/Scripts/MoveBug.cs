35a598777df566fd0ab7307040273efacbe6abe3
Cole Cecil <cole.cecil@gmail.com>
2023-02-04 22:55:35 -0600

    Fix collision issues?

--- Assets/Scripts/MoveBug.cs
+++ Assets/Scripts/MoveBug.cs
@@ -5,13 +5,16 @@ public class MoveBug : MonoBehaviour
 {
     private readonly float maxSpeed = 12f;
     private readonly float acceleration = 5f;
-    private readonly float spinSpeed = 30f;
+    private readonly float maxSpinSpeed = 60f;
 
+    private Rigidbody bugRigibody;
     private Transform bugTransform;
     private float speed = 0f;
+    private float spinSpeed = 0f;
 
     void Start()
     {
+	    bugRigibody = gameObject.GetComponent<Rigidbody>();
 	    bugTransform = gameObject.GetComponent<Transform>();
     }
     
@@ -26,15 +29,23 @@ public class MoveBug : MonoBehaviour
 			speed = Mathf.Clamp(speed - 2f * acceleration * Time.deltaTime, 0f, maxSpeed);
 		}
 
-		bugTransform.position += bugTransform.rotation * Vector3.back * (speed * Time.deltaTime);
-
 		if (Input.GetAxis("Horizontal") > 0f)
 		{
-			bugTransform.Rotate(0f, spinSpeed * Time.deltaTime, 0f);
+			spinSpeed = maxSpinSpeed;
 		}
 		else if (Input.GetAxis("Horizontal") < 0f)
 		{
-			bugTransform.Rotate(0f, -spinSpeed * Time.deltaTime, 0f);
+			spinSpeed = -maxSpinSpeed;
 		}
+		else
+		{
+			spinSpeed = 0;
+		}
+    }
+
+    void FixedUpdate()
+    {
+		bugRigibody.MoveRotation(bugTransform.rotation * Quaternion.Euler(0f, spinSpeed * Time.deltaTime, 0f));
+		bugRigibody.MovePosition(bugTransform.position + bugTransform.rotation * Vector3.back * (speed * Time.deltaTime));
     }
 }
